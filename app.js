0630f43f9eec6bcc9930378a42a5a2879ef30690
Marian Kuna <mkunask@gmail.com>
2018-04-16 13:51:17 +0200

    Removed Bratislava

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var cities = {cities:["Bratislava","Amsterdam","Berlin","New York","San Francisco","Tokyo"]}
+var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo"]}
 
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
